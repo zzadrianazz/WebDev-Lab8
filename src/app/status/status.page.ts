cdb92235d6be075d94e0a25844f5c926c9f7c27d
Adriana Christ <adrianachrist9@gmail.com>
2026-04-18 21:53:09 +0100

    Update status.page.ts

--- src/app/status/status.page.ts
+++ src/app/status/status.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonRadioGroup, IonItem, IonLabel, IonRadio, IonButton } from '@ionic/angular/standalone';
-import { Storage } from '@ionic/storage-angular';
+import { Storage } from '@ionic/storage-angular'; //import ionic storage for saving data locally
 
 @Component({
   selector: 'app-status',
@@ -12,13 +12,14 @@ import { Storage } from '@ionic/storage-angular';
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonRadioGroup, IonItem, IonLabel, IonRadio, IonButton]
 })
 export class StatusPage implements OnInit {
-  status:string = "";
-  constructor(private storage:Storage) { }
+  status:string = ""; //store selected status
+  constructor(private storage:Storage) { } //ionic storage service into component
 
+  //method to save selected status to local storage
   async saveStatus(){
     console.log(this.status);
-    await this.storage.create();
-    await this.storage.set('myStatus', this.status);
+    await this.storage.create(); //makes sure the storage is initialised
+    await this.storage.set('myStatus', this.status); //saves the status under 'myStatus' key
   }
 
   ngOnInit() {
@@ -26,7 +27,7 @@ export class StatusPage implements OnInit {
 
   async ionViewWillEnter(){
     await this.storage.create();
-    this.status = await this.storage.get('myStatus')
+    this.status = await this.storage.get('myStatus') //retrieves saved status from storage and assigns it to variable
   }
 
 }
